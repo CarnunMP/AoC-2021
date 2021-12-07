a1201c6f74aae42cd37a547ea8bc2aad6c535be4
CarnunMP <carnun@hotmail.co.uk>
2021-12-07 23:43:16 +0000

    Day 7: (2/2)

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Advent of Code, 2021
 
-| Day | My Answer |
+| day | quality of my answer |
 | --- | --- |
 | [1](src/days/day1.clj) | alright |
 | [2](src/days/day2.clj) | kinda dumb |
@@ -8,5 +8,6 @@
 | [4](src/days/day4.clj) | mega dumb | 
 | [5](src/days/day5.clj) | not bad | 
 | [6](src/days/day6.clj) | ditto! |
+| [7](src/days/day7.clj) | could be worse |
 
 (feedback welcome!)
--- src/days/day7.clj
+++ src/days/day7.clj
@@ -11,11 +11,12 @@
 
 (defn parse-input [input]
   (->> (str/split input #",")
-       (map #(Integer/parseInt %))))
+       (map #(Integer/parseInt %))
+       sort
+       vec))
 
 (defn assoc-median [nums]
-  (let [nums (vec (sort nums))
-        middle-index (dec (/ (count nums) 2))]
+  (let [middle-index (dec (/ (count nums) 2))]
     {:nums nums
      :medians (if (int? middle-index)
                (subvec nums middle-index (+ 2 middle-index))
@@ -37,3 +38,39 @@
 
 (part-1 dummy-input) ; => 37
 (part-1 input) ; => 328262
+
+
+;; Part 2
+
+; before: minimise sum of ds, where d is distance
+; now: minimise sum of t(d) = d(d + 1)/2, as t(d) is a https://en.wikipedia.org/wiki/Triangular_number
+
+; hmm. I can't think of anything cleverer than just computing this for a bunch of positions...
+; and maybe stopping once the _next_ position has a fuel use greater than the previous—cos there will be a global minimum!
+; there's a symmetry to things after all
+
+
+(defn- fuel-use [n candidate-pos]
+  (let [d (Math/abs (- n candidate-pos))]
+    (/ (* d (inc d)) 2)))
+
+(defn- total-fuel-use [nums candidate-pos]
+  (reduce #(+ %1 (fuel-use %2 candidate-pos))
+          0 nums))
+
+(defn find-best-total-fuel-use [nums]
+  (reduce
+    (fn [best candidate-pos]
+      (let [current (total-fuel-use nums candidate-pos)]
+        (if (<= current best)
+          current
+          (reduced best))))
+    ##Inf
+    (range (first nums) (inc (peek nums)))))
+
+(defn part-2 [input]
+  (-> input
+      parse-input
+      find-best-total-fuel-use))
+
+(part-2 input) ; => 90040997
