e2fc01826b4d9c1e11150d14fa934721bb360475
CarnunMP <carnun@hotmail.co.uk>
2021-12-11 14:22:29 +0000

    Day 10: (2/2)

--- src/days/day10.clj
+++ src/days/day10.clj
@@ -31,7 +31,8 @@
                           \} 1197
                           \> 25137})
 
-(defn get-illegal-chars [input]
+(defn get-illegal-or-completion-chars
+  [input & {:keys [completion?] :or {completion? false}}]
   (letfn [(illegal-char [s]
             (reduce
               (fn [stack c]
@@ -44,8 +45,9 @@
                         (reduced c))))))
               '()
               s))]
-    (->> (map illegal-char input)
-         (remove seq?))))
+    (cond->> (map illegal-char input)
+      (not completion?) (remove seq?)
+      completion? (remove char?))))
 
 (defn score [illegal-chars]
   (reduce
@@ -56,7 +58,38 @@
 
 (defn part-1 [input]
   (-> input
-      get-illegal-chars
+      get-illegal-or-completion-chars
       score))
 
 (part-1 input) ; => 362271
+
+
+;; Part 2
+
+(def completion-char->score {\) 1
+                             \] 2
+                             \} 3
+                             \> 4})
+
+(defn score-completions [completions]
+  (letfn [(score [cs]
+            (reduce
+              (fn [total c]
+                (+ (* 5 total) (completion-char->score c)))
+              0
+              cs))]
+    (map score completions)))
+
+(defn get-middle-score [scores]
+  (let [n (int (/ (count scores) 2))]
+    (nth scores n)))
+
+(defn part-2 [input]
+  (-> input
+      (get-illegal-or-completion-chars :completion? true)
+      score-completions
+      sort
+      get-middle-score))
+
+(part-2 dummy-input) ; => 288957
+(part-2 input) ; => 1698395182
