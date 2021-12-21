e3e539970b7a68aad1e86fe7d390c5bfeded64e6
CarnunMP <admin@carnun.com>
2021-12-21 16:12:57 +0000

    Day 12: (2/2)

--- src/days/day12.clj
+++ src/days/day12.clj
@@ -41,9 +41,11 @@
     input))
 
 ; recursive DFS!
+(defn- small-cave? [cave]
+  (not= cave (.toUpperCase cave)))
+
 (defn generate-legal-paths [graph]
-  (letfn [(small-cave? [cave] (not= cave (.toUpperCase cave)))
-          (traverse [graph path visited?]
+  (letfn [(traverse [graph path visited?]
             (let [cave (peek path)
                   neighbours (remove visited? (get graph cave))]
               (if (= "end" cave)
@@ -67,3 +69,45 @@
 (part-1 dummy-input-1) ; => 10
 (part-1 dummy-input-2) ; => 19
 (part-1 input) ; => 3510
+
+
+;; Part 2
+
+; hmm... I guess in addition to a visited? set I need to keep track of how many times small caves have been visited
+(defn generate-legal-paths' [graph]
+  (letfn [(traverse [graph path visited? small-cave->visits]
+            (let [cave (peek path)
+                  small-cave->visits (if (small-cave? cave)
+                                       (update small-cave->visits cave inc)
+                                       small-cave->visits)
+                  visit-limit? (some #(<= 2 %) (vals small-cave->visits))
+                  visited? (if visit-limit?
+                             (apply conj visited? (keep (fn [[cave visits]]
+                                                          (when (pos? visits)
+                                                            cave))
+                                                        small-cave->visits))
+                             visited?)
+                  neighbours (remove visited? (get graph cave))]
+              ;(prn path visited? small-cave->visits visit-limit?)
+              (if (= "end" cave)
+                [path]
+                (mapcat (fn [neighbour]
+                          (traverse
+                            graph
+                            (conj path neighbour)
+                            visited?
+                            small-cave->visits))
+                        neighbours))))]
+    (traverse graph ["start"] #{"start"} (zipmap
+                                           (filter small-cave? (keys graph))
+                                           (repeat 0)))))
+
+(defn part-2 [input]
+  (-> input
+      parse-input
+      generate-legal-paths'
+      count))
+
+(part-2 dummy-input-1) ; => 36
+(part-2 dummy-input-2) ; => 103
+(part-2 input) ; => 122880
